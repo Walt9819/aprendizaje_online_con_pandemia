635d5adcde4ad531001460527ae2140b81148fbf
Walt9819 <walter22r@icloud.com>
2021-08-23 00:35:49 -0500

    Add all covid daily cases, deaths, active and recovered

--- transform.py
+++ transform.py
@@ -13,7 +13,7 @@ onlineData = online_learning[["district_id", "state"]]
 onlineData.loc[:, ["state"]] = onlineData["state"].replace(np.nan, "Unknown")
 
 ## Create new dataframe where all information will be placed
-allData = pd.DataFrame(columns=["state", "date", "pct_access", "engagement_index"])
+allOnline = pd.DataFrame(columns=["state", "date", "pct_access", "engagement_index"])
 
 # For each engagement file, add their info to the df
 for file in os.listdir("rawData/onlinelearning/engagement_data"):
@@ -32,4 +32,25 @@ for file in os.listdir("rawData/onlinelearning/engagement_data"):
     cols[0] = "date"
     aggData.columns = cols
     aggData["state"] = state # add state to df
-    allData = allData.append(aggData) # add values to df
+    allOnline = allOnline.append(aggData) # add values to df
+
+
+#### COVID-19 cases ####
+## Create new dataframe where all information will be placed
+allCases = pd.DataFrame(columns=["state", "date", "confirmed", "deaths", "recovered", "active"])
+
+# for each file load daily statistics and add to main df
+for file in os.listdir("rawData/csse_covid_19_daily_reports_us"):
+    # avoid README file
+    if file.split('.')[1] != 'csv':
+        continue
+    date = pd.to_datetime((file.split('.')[0])) # convert filename to date
+    data = pd.read_csv(f"rawData/csse_covid_19_daily_reports_us/{file}")
+    # Select desired columns
+    data = data[["Province_State", "Confirmed", "Deaths", "Recovered", "Active"]]
+    data["date"] = date.strftime("%m-%d_%Y") # add date as column
+    data.columns = ["state", "confirmed", "deaths", "recovered", "active", "date"]
+    if "Recovered" in data["state"]:
+        print(data)
+    allCases = allCases.append(data) # add rows to df
+
